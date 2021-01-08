10d2a0b8644f8b9b195c4db099e8393f04a6cb06
Ivan Chiang <ivan21916@gmail.com>
2021-01-08 03:40:14 -0800

    Create Moving Vehicle And Applying Physical Functions

---
+++ Source/KrazyKarts/GoKart.cpp
@@ -0,0 +1,98 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GoKart.h"
+#include "Components/InputComponent.h"
+#include "Engine/World.h"
+
+// Sets default values
+AGoKart::AGoKart()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AGoKart::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AGoKart::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	FVector Force = GetActorForwardVector() * MaxDrivingForce * Throttle;
+	Force += GetAirResistance();
+	Force += GetRollingResistance();
+	
+	FVector Acceleration = Force / Mass;
+
+	float AccelerationDueToGravity = - GetWorld()->GetGravityZ() / 100;
+
+	Velocity = Velocity + Acceleration * DeltaTime;
+
+	ApplyRotation(DeltaTime);
+
+	UpdateLocationFromVelocity(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AGoKart::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &AGoKart::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AGoKart::MoveRight);
+}
+
+FVector AGoKart::GetAirResistance() 
+{
+	return - Velocity.GetSafeNormal() * Velocity.SizeSquared() * DragCoefficient;
+}
+
+FVector AGoKart::GetRollingResistance() 
+{	
+	float AccelerationDueToGravity = - GetWorld()->GetGravityZ() / 100;
+	float NormalForce = Mass * AccelerationDueToGravity;
+	return - Velocity.GetSafeNormal() * RollingResistanceCoefficient * NormalForce;
+}
+
+void AGoKart::UpdateLocationFromVelocity(float DeltaTime) 
+{
+	FVector Translation = Velocity * DeltaTime * 100;
+
+	FHitResult Hit;
+	AddActorWorldOffset(Translation, true, &Hit);
+
+	if(Hit.IsValidBlockingHit())
+	{
+		Velocity = FVector::ZeroVector;
+	}
+}
+
+void AGoKart::ApplyRotation(float DeltaTime) 
+{	
+	float DeltaLocation = FVector::DotProduct(GetActorForwardVector(), Velocity) * DeltaTime;
+	float RotationAngle = (DeltaLocation / MinTurningRadius) * SteeringThrow;
+	FQuat RotationDelta(GetActorUpVector(), RotationAngle);
+
+	Velocity = RotationDelta.RotateVector(Velocity);
+
+	AddActorWorldRotation(RotationDelta);	
+}
+
+void AGoKart::MoveForward(float Value) 
+{
+	Throttle = Value;
+}
+
+void AGoKart::MoveRight(float Value) 
+{
+	SteeringThrow = Value;
+}
+
---
+++ Source/KrazyKarts/GoKart.h
@@ -0,0 +1,67 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "GoKart.generated.h"
+
+UCLASS()
+class KRAZYKARTS_API AGoKart : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	AGoKart();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+	FVector GetAirResistance();
+
+	FVector GetRollingResistance();
+
+	void UpdateLocationFromVelocity(float DeltaTime);
+
+	void ApplyRotation(float DeltaTime);
+
+	void MoveForward(float Value);
+
+	void MoveRight(float Value);
+
+	FVector Velocity;
+
+	// The mass of the car (kg)
+	UPROPERTY(EditAnywhere)
+	float Mass = 1000;
+
+	// The force applied to the car when the throttle is fully down (N)
+	UPROPERTY(EditAnywhere)
+	float MaxDrivingForce = 10000;
+
+	//Minimum radius of the car turning circle at full lock (m)
+	UPROPERTY(EditAnywhere)
+	float MinTurningRadius = 10;
+
+	//Higher means more drag
+	UPROPERTY(EditAnywhere)
+	float DragCoefficient = 16;
+
+	//Higher means more rolling resistance
+	UPROPERTY(EditAnywhere)
+	float RollingResistanceCoefficient = 0.015;
+
+	float Throttle;
+	float SteeringThrow;
+
+};
